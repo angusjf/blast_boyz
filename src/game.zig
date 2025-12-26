3ecf16e879b4f64424f5018b5aa183c67fc929a2
Angus Findlay <angus.findlay@icloud.com>
2025-12-26 16:42:03 +0000

    multiplayer

---
+++ src/game.zig
@@ -0,0 +1,645 @@
+const rl = @import("raylib");
+const std = @import("std");
+
+const PlayerSprite = struct {
+    norm: u8,
+    jump: u8,
+    fall: u8,
+    down: u8,
+};
+
+pub const Buttons = struct {
+    left_hit: bool = false,
+    right_hit: bool = false,
+    up_hit: bool = false,
+    x_hit: bool = false,
+    left_held: bool = false,
+    right_held: bool = false,
+    up_held: bool = false,
+    x_held: bool = false,
+};
+
+const player_sprites = [2]PlayerSprite{
+    .{
+        .norm = 5,
+        .jump = 21,
+        .fall = 37,
+        .down = 53,
+    },
+    .{
+        .norm = 6,
+        .jump = 22,
+        .fall = 38,
+        .down = 54,
+    },
+};
+
+const bomb_spr = 3;
+const bomb_red_spr = 19;
+
+const bg_sprite = 4;
+
+const block_sprite = 2;
+
+const grav = 0.35;
+const jump = 4.5;
+const throw = 4;
+const walk = 20;
+const fric = 0.1;
+const gem_fric = 0.95;
+const bomb_life = 240;
+
+const State = struct {
+    players: [2]Player,
+    ticks: u16,
+    paused: bool,
+    active: u1,
+    blocks: std.ArrayList(Vec2),
+    bombs: std.ArrayList(Bomb),
+    particles: std.ArrayList(Particle),
+    shake: f32,
+};
+
+var sheet: ?rl.Texture2D = null;
+
+const Player = struct {
+    vx: f32,
+    vy: f32,
+    flip: bool,
+    x: f32,
+    y: f32,
+    grounded: bool,
+};
+
+var state: State = undefined;
+
+const Vec2 = struct { x: f32, y: f32 };
+
+const Bomb = struct { x: f32, y: f32, vx: f32, vy: f32, t: u16 };
+
+const Particle = struct {
+    type: u16,
+    t: u16,
+    x: f32,
+    y: f32,
+    vx: f32,
+    vy: f32,
+    r: f32,
+    c: u16,
+};
+
+var blocks_buf: [64]Vec2 = undefined;
+var bombs_buf: [64]Bomb = undefined;
+var particles_buf: [128]Particle = undefined;
+
+var rand: std.Random = undefined;
+
+fn lerp(a: f32, b: f32, t: f32) f32 {
+    return a * (1 - t) + b * t;
+}
+
+var palette: [16]rl.Color = undefined;
+
+pub fn init(active: u1) !void {
+    var prng = std.Random.Xoshiro256.init(blk: {
+        var seed: u64 = undefined;
+        std.posix.getrandom(std.mem.asBytes(&seed)) catch @panic("random setup failed");
+        break :blk seed;
+    });
+
+    rand = prng.random();
+
+    palette = .{
+        rl.Color.fromInt(0x000000), // black
+        rl.Color.fromInt(0x1D2B53), // dark-blue
+        rl.Color.fromInt(0x7E2553), // dark-purple
+        rl.Color.fromInt(0x008751), // dark-green
+        rl.Color.fromInt(0xAB5236), // brown
+        rl.Color.fromInt(0x5F574F), // dark-grey
+        rl.Color.fromInt(0xC2C3C7), // light-grey
+        rl.Color.fromInt(0xFFF1E8), // white
+        rl.Color.fromInt(0xFF004D), // red
+        rl.Color.fromInt(0xFFA300), // orange
+        rl.Color.fromInt(0xFFEC27), // yellow
+        rl.Color.fromInt(0x00E436), // green
+        rl.Color.fromInt(0x29ADFF), // blue
+        rl.Color.fromInt(0x83769C), // lavender
+        rl.Color.fromInt(0xFF77A8), // pink
+        rl.Color.fromInt(0xFFCCAA), // light-peach
+    };
+
+    state = .{
+        .players = [2]Player{
+            .{ .vx = 0, .vy = -1, .flip = false, .x = 8, .y = 23, .grounded = false },
+            .{ .vx = 0, .vy = -1, .flip = true, .x = 112, .y = 10, .grounded = false },
+        },
+        .ticks = 0,
+        .paused = true,
+        .active = active,
+        .blocks = .initBuffer(&blocks_buf),
+        .bombs = .initBuffer(&bombs_buf),
+        .particles = .initBuffer(&particles_buf),
+        //
+        // .active = 1
+        .shake = 0,
+    };
+
+    for (0..16) |x| {
+        for (0..16) |y| {
+            const m = mget(x, y);
+            if (m == block_sprite) {
+                state.blocks.appendAssumeCapacity(.{
+                    .x = @floatFromInt(x * 8),
+                    .y = @floatFromInt(y * 8),
+                });
+            } else if (m >= 7 and m <= 11) {
+                // add(gems, { m=m, x=x*8, y=y*8, vx=0, vy=0 })
+            }
+        }
+    }
+}
+
+fn mget(x: usize, y: usize) u16 {
+    const map =
+        \\0000000000000000
+        \\0000000000000000
+        \\0000000000000000
+        \\0000111000000000
+        \\0000000000000000
+        \\0000000000000111
+        \\0000000000000000
+        \\0000000000000000
+        \\1110000000000000
+        \\0000000000111100
+        \\0000000000000000
+        \\0000000000000000
+        \\1111000000000000
+        \\0000000000000000
+        \\0000000011110000
+        \\0000000000000000
+        \\0000000000000000
+        \\0000000000000000
+        \\
+    ;
+
+    return switch (map[y * 17 + x]) {
+        '1' => block_sprite,
+        else => 0,
+    };
+}
+
+fn control(p: *Player, buttons: Buttons) void {
+    if (buttons.left_held) {
+        p.vx = -walk;
+        p.flip = true;
+    } else if (buttons.right_held) {
+        p.vx = walk;
+
+        p.flip = false;
+    }
+
+    if (buttons.up_hit and p.vy == 0) {
+        p.vy = -jump;
+        sfx(0);
+    }
+
+    if (buttons.x_hit) {
+        if (true) {
+            // if (p.carry) {
+            //      sfx(6)
+            //
+            //      for gem in all(gems) do
+            //        if gem.m == p.carry then
+            //          g = gem
+            //        end
+            //      end
+            //      if p.carry == "bomb" then
+            //        g = bombs[1]
+            //      end
+            //
+            //      if p.flip then
+            //        dx = -throw
+            //      else
+            //        dx = throw
+            //      end
+            //
+            //      if not btn(0)
+            //         and not btn(1) then
+            //        dx /= 10
+            //      end
+            //
+            //      g.vy += -throw + p.vy / 15
+            //      g.vx = dx + p.vx / 15
+            //
+            //      p.carry = nil
+        } else {
+            //       g = gem_near(p.x, p.y)
+            //       if g then
+            //         sfx(2)
+            //         if players[1].carry == g.m then
+            //           players[1].carry = nil
+            //         elseif players[2].carry == g.m then
+            //           players[2].carry = nil
+            //         end
+            //         p.carry = g.m
+            //       else
+            //         b = bomb_near(p.x, p.y)
+            //         if (b) then
+            //           sfx(2)
+            //           if players[1].carry == "bomb" then
+            //             players[1].carry = nil
+            //           elseif players[2].carry == "bomb" then
+            //             players[2].carry = nil
+            //           end
+            //           p.carry = "bomb"
+            //         end
+            //       end
+        }
+    }
+}
+
+fn rnd(max: f32) f32 {
+    return rand.float(f32) * max;
+}
+
+fn rndInt(max: u16) u16 {
+    return rand.intRangeAtMost(u16, 0, max);
+}
+
+fn rndChoose(options: []const f32) f32 {
+    return options[rand.int(usize) % options.len];
+}
+
+pub fn update(user_buttons: Buttons, network_buttons: Buttons) !void {
+
+    // shake = lerp(shake, 0, 0.3)
+    //
+    // if paused then
+    //   if btn(0) and btn(1)
+    //     and btn(2) and btn(4) then
+    //     paused = false
+    //     sfx(3)
+    //     return
+    //   else
+    //     return
+    //   end
+    // end
+    //
+    state.ticks +%= 1;
+    //
+    // if ticks % 300 == 299 then
+    //   if active == 1 then
+    //     active = 2
+    //   else
+    //     active = 1
+    //   end
+    //   paused=true
+    // end
+    //
+    if (state.ticks % 100 == 99 and state.bombs.items.len == 0)
+        state.bombs.appendAssumeCapacity(.{
+            .x = 10 + rnd(100),
+            .y = 0,
+            .vx = 0,
+            .vy = 0,
+            .t = 0,
+        });
+
+    for (state.bombs.items) |bomb| {
+        if (state.ticks % 10 == 0) {
+            state.particles.appendAssumeCapacity(.{
+                .type = 0,
+                .t = rndInt(15),
+                .x = bomb.x + rnd(5),
+                .y = bomb.y + rnd(5) - 2,
+                .vx = rnd(1) - 1 / 2,
+                .vy = rnd(1) - 1 / 2,
+                .r = rnd(2),
+                .c = @intFromFloat(rndChoose(&[_]f32{ 5, 9, 10, 13 })),
+            });
+        }
+    }
+
+    control(&state.players[state.active], user_buttons);
+    control(&state.players[
+        switch (state.active) {
+            0 => 1,
+            1 => 0,
+        }
+    ], network_buttons);
+
+    for (&state.players) |*player| {
+        player.vy += grav;
+        player.vx *= fric;
+
+        if (player.x + player.vx < 0) {
+            player.x = 0 - player.vx;
+            player.vx *= -1;
+        } else if (player.x + player.vx > 128 - 8) {
+            player.x = 128 - 8;
+            player.vx *= -1;
+        } else {
+            player.x += player.vx;
+        }
+
+        const d2b = dist_to_block(player.x, player.y);
+
+        if (player.vy > d2b) {
+            player.y += d2b;
+            player.vy = 0;
+            if (!player.grounded) {
+                sfx(4);
+                player.grounded = true;
+            }
+        } else {
+            player.y += player.vy;
+        }
+
+        if (player.vy != 0) {
+            player.grounded = false;
+        }
+
+        player.y = @mod(player.y + 128, 128);
+    }
+
+    for (state.bombs.items) |*bomb| {
+        if (bomb.t == bomb_life) {
+            explode(bomb);
+        }
+
+        //
+        //   if players[1].carry == "bomb" then
+        //     bomb.vx = 0
+        //     bomb.vy = 0
+        //     bomb.x = players[1].x
+        //     bomb.y = players[1].y - 7
+        //   elseif players[2].carry == "bomb" then
+        //     bomb.vx = 0
+        //     bomb.vy = 0
+        //     bomb.x = players[2].x
+        //     bomb.y = players[2].y - 7
+        //   else
+        bomb.vy += grav;
+        bomb.vx *= gem_fric;
+
+        if (bomb.x + bomb.vx < 0) {
+            bomb.x = 0 - bomb.vx;
+            bomb.vx *= -1;
+            state.shake = 2;
+        } else if (bomb.x + bomb.vx > 128 - 8) {
+            bomb.x = 128 - bomb.vx - 8;
+            bomb.vx *= -1;
+            state.shake = 2;
+        } else {
+            bomb.x += bomb.vx;
+        }
+
+        const d2b = dist_to_block(bomb.x, bomb.y);
+
+        if (bomb.vy > d2b) {
+            if (bomb.vy > 0.5) {
+                sfx(7);
+            }
+            bomb.y += d2b;
+            bomb.vy = -bomb.vy / 4;
+            bomb.vx = bomb.vy / 2 * (rnd(1) - 0.5);
+        } else {
+            bomb.y += bomb.vy;
+        }
+        //   end
+        //
+        bomb.t += 1;
+    }
+
+    for (state.particles.items) |*s| {
+        if (s.t <= 0) {
+            // del(sparks, s);
+        } else {
+            s.x += s.vx;
+            s.y += s.vy;
+            s.t -= 1;
+        }
+    }
+}
+
+fn circfill(x: f32, y: f32, r: f32, c: u16) void {
+    rl.drawCircleSector(
+        rl.Vector2{ .x = x, .y = y },
+        r,
+        0,
+        360,
+        5,
+        palette[c],
+    );
+}
+
+pub fn draw() !void {
+    rl.clearBackground(.white);
+
+    //   palt()
+    //
+    //   if shake > 0.1 then
+    //     camera(rnd(shake) - shake/2,
+    //            rnd(shake) - shake/2)
+    //   else
+    //     camera(0, 0)
+    //     camera(0, 0)
+    //   end
+    //
+    // for 0,15 do
+    //   for y=0,15 do
+    //     spr(bg_sprite, x * 8, y * 8)
+    //   end
+    // end
+    //
+
+    for (state.particles.items) |s| {
+        circfill(s.x, s.y, s.r, s.c);
+    }
+    //
+    //   palt(0b0000000100000000)
+    //
+    for (state.blocks.items) |block| {
+        spr(block_sprite, block.x, block.y, false, false);
+    }
+    //
+    //
+    for (0..2) |p| {
+        const player_sprite = if (state.players[p].vy < 0)
+            player_sprites[p].jump
+        else
+            (if (state.players[p].vy > 0)
+                player_sprites[p].fall
+            else
+                player_sprites[p].norm);
+
+        spr(
+            player_sprite,
+            state.players[p].x,
+            state.players[p].y,
+            state.players[p].flip,
+            false,
+        );
+    }
+    //
+    //   palt(0b0000100000000000)
+    //
+    //   gsp = ticks \ 3
+    //
+    //   for _,gem in ipairs(gems) do
+    //     gem_spr = gem.m
+    //     if gsp % 12 > 8 then
+    //       gem_spr += 16 * ((gsp % 12) - 8)
+    //     end
+    //     spr(gem_spr, gem.x, gem.y)
+    //   end
+    //
+    //   palt(12)
+    //
+    for (state.bombs.items) |bomb| {
+        var mod: u16 = undefined;
+        if (bomb.t > bomb_life * 0.8) {
+            mod = 5;
+        } else if (bomb.t > bomb_life * 0.6) {
+            mod = 10;
+        } else if (bomb.t > bomb_life * 0.4) {
+            mod = 20;
+        } else {
+            mod = 100;
+        }
+
+        var bomb_anim: u8 = bomb_spr;
+        if ((state.ticks / mod) % 2 != 1) {
+            bomb_anim = bomb_red_spr;
+        }
+
+        spr(bomb_anim, bomb.x, bomb.y, false, false);
+    }
+    //
+    //   if paused then
+    //     if active == 1 then
+    //       c = 8
+    //     else
+    //       c = 12
+    //     end
+    //     rectfill(38, 48, 96, 56, 0)
+    //     print("swap to plyr " .. active,
+    //           40, 50, c)
+    //
+    //     rectfill(38, 58, 96, 66, 0)
+    //     print("press 🅾️⬅️⬆️➡️",
+    //           40, 60, c)
+    //   end
+
+}
+
+fn sfx(n: u8) void {
+    const data: []const u8 = switch (n) {
+        0 => @embedFile("assets/mygame_sfx_0.wav"),
+        1 => @embedFile("assets/mygame_sfx_1.wav"),
+        4 => @embedFile("assets/mygame_sfx_4.wav"),
+        7 => @embedFile("assets/mygame_sfx_7.wav"),
+        else => unreachable,
+    };
+
+    const wave = rl.loadWaveFromMemory(".wav", data) catch unreachable;
+
+    const sound = rl.loadSoundFromWave(wave);
+
+    rl.unloadWave(wave); // IMPORTANT: sound has its own copy now
+
+    rl.playSound(sound);
+}
+
+fn spr(n: u8, x: f32, y: f32, flip_x: bool, flip_y: bool) void {
+    const sprite_size = 8;
+
+    if (sheet == null) {
+        const image = rl.loadImageFromMemory(
+            ".png",
+            @embedFile("assets/spritesheet.png"),
+        ) catch unreachable;
+        sheet = rl.loadTextureFromImage(image) catch unreachable;
+    }
+
+    const source: rl.Rectangle = .{
+        .x = @as(f32, @floatFromInt(n)) * sprite_size,
+        .y = 0,
+        .width = if (flip_x) -sprite_size else sprite_size,
+        .height = if (flip_y) -sprite_size else sprite_size,
+    };
+
+    const dest = rl.Vector2{ .x = x, .y = y };
+
+    rl.drawTextureRec(
+        sheet.?,
+        source,
+        dest,
+        rl.Color.white,
+    );
+}
+
+fn explode(bomb: *Bomb) void {
+    _ = bomb; // autofix
+    sfx(1);
+    //   shake = 10
+    //   del(bombs, bomb)
+    //
+    //   if players[1].carry == "bomb" then
+    //     players[1].carry = nil
+    //   end
+    //   if players[2].carry == "bomb" then
+    //     players[2].carry = nil
+    //   end
+    //
+    //   function bash(obj, force)
+    //     dx = obj.x - bomb.x
+    //     dy = obj.y - bomb.y
+    //     dist = sqrt(dx*dx + dy*dy) + 1
+    //     obj.vx += sgn(dx) * force / dist
+    //     obj.vy += sgn(dy - 1) * force / dist
+    //   end
+    //
+    //   for player in all(players) do
+    //     bash(player, 10)
+    //   end
+    //   for gem in all(gems) do
+    //     bash(gem, 20)
+    //   end
+    //
+    //   for i = 1,20 do
+    //     add(smoke, {
+    //       t = rnd(50),
+    //       x = bomb.x + rnd(10) - 5,
+    //       y = bomb.y + rnd(10) - 5,
+    //       vx = rnd(2) - 1,
+    //       vy = rnd(2) - 1,
+    //       r = rnd(5),
+    //       c = rnd({5,6,7,13})
+    //     })
+    //   end
+}
+
+//
+// function bomb_near(x, y)
+//   for bomb in all(bombs) do
+//     if x > bomb.x - 8
+//       and x < bomb.x + 8
+//       and y > bomb.y - 8
+//       and y < bomb.y + 8 then
+//       return bomb
+//     end
+//   end
+// end
+
+fn dist_to_block(x: f32, y: f32) f32 {
+    var d2b: f32 = 999;
+
+    for (state.blocks.items) |block| {
+        if (x > block.x - 8 and x < block.x + 8 and y + 8 <= block.y) {
+            d2b = @min(d2b, block.y - (y + 8));
+        }
+    }
+
+    return d2b;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,267 @@
 const std = @import("std");
-const pico_raylib = @import("pico_raylib");
+const rl = @import("raylib");
+const game = @import("game.zig");
+
+const PORT: u16 = 50000;
+
+fn broadcastDiscovery(fd: std.posix.fd_t) !void {
+    std.debug.print("sending discovery request to 255.255.255.255\n", .{});
+
+    var broadcast = std.posix.sockaddr.in{
+        .family = std.posix.AF.INET,
+        .port = std.mem.nativeToBig(u16, PORT),
+        .addr = 0xFFFFFFFF,
+        .zero = .{0} ** 8,
+    };
+
+    const req = "slim easy";
+
+    _ = try std.posix.sendto(
+        fd,
+        req,
+        0,
+        @ptrCast(&broadcast),
+        @sizeOf(std.posix.sockaddr.in),
+    );
+}
+
+fn bindSocket(fd: std.posix.fd_t, port: u16) !void {
+    var addr = std.posix.sockaddr.in{
+        .family = std.posix.AF.INET,
+        .port = std.mem.nativeToBig(u16, port),
+        .addr = 0, //std.posix.in_addr{ .s_addr = 0 },
+        .zero = .{0} ** 8,
+    };
+
+    try std.posix.bind(
+        fd,
+        @ptrCast(&addr),
+        @sizeOf(std.posix.sockaddr.in),
+    );
+}
+
+var is_connected = false;
+
+fn getNetworkButtons(fd: std.posix.fd_t, network_keys: *game.Buttons) void {
+    var from: std.posix.sockaddr.in = undefined;
+    var from_len: std.posix.socklen_t = @sizeOf(std.posix.sockaddr.in);
+
+    _ = std.posix.recvfrom(
+        fd,
+        std.mem.asBytes(network_keys),
+        0,
+        @ptrCast(&from),
+        &from_len,
+    ) catch |err| switch (err) {
+        error.WouldBlock => return,
+        else => {
+            std.debug.print("recv error: {}\n", .{err});
+            return;
+        },
+    };
+
+    std.debug.print("{any}\n", .{network_keys});
+}
+
+fn listenForFriend(fd: std.posix.fd_t) ?std.posix.sockaddr.in {
+    var from: std.posix.sockaddr.in = undefined;
+    var from_len: std.posix.socklen_t = @sizeOf(std.posix.sockaddr.in);
+
+    var buf: [512]u8 = undefined;
+    const n = std.posix.recvfrom(
+        fd,
+        &buf,
+        0,
+        @ptrCast(&from),
+        &from_len,
+    ) catch |err| switch (err) {
+        error.WouldBlock => return null,
+        else => {
+            std.debug.print("recv error: {}\n", .{err});
+            @panic("ERROR RECV");
+        },
+    };
+    std.debug.print("{any}\n", .{buf[0..n]});
+
+    return if (std.mem.eql(u8, buf[0..n], "lesgo"))
+        from
+    else
+        null;
+}
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try pico_raylib.bufferedPrint();
-}
+    var args = std.process.args();
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    _ = args.skip();
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    const player: u1 = if (args.next()) |arg| p: {
+        if (std.mem.eql(u8, arg, "p1")) break :p 0;
+        if (std.mem.eql(u8, arg, "p2")) break :p 1;
+        @panic("must be p1 or p2");
+    } else @panic("must give arg");
+
+    std.debug.print("{d}", .{player});
+
+    const scale = 4;
+    const width = 128;
+    const height = 128;
+
+    rl.initWindow(width * scale, width * scale, "hello world :)");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+
+    const target: rl.RenderTexture2D = try rl.loadRenderTexture(width, height);
+    rl.setTextureFilter(target.texture, rl.TextureFilter.point);
+    rl.initAudioDevice();
+
+    // get a socket and set domain, type and protocol flags
+    const sock = try std.posix.socket(
+        std.posix.AF.INET,
+        std.posix.SOCK.DGRAM,
+        std.posix.IPPROTO.UDP,
+    );
+
+    defer std.posix.close(sock);
+
+    {
+        var flags: std.posix.O = @bitCast(@as(u32, @truncate(
+            try std.posix.fcntl(sock, std.posix.F.GETFL, 0),
+        )));
+
+        // modify
+        flags.NONBLOCK = true;
+
+        _ = try std.posix.fcntl(
+            sock,
+            std.posix.F.SETFL,
+            @intCast(@as(u32, @bitCast(flags))),
+        );
+    }
+
+    if (player == 0) {
+        std.debug.print("putting socket in broadcast mode.\n", .{});
+
+        const yes: c_int = 1;
+        try std.posix.setsockopt(
+            sock,
+            std.posix.SOL.SOCKET,
+            std.posix.SO.BROADCAST,
+            std.mem.asBytes(&yes),
+        );
+    }
+
+    try bindSocket(sock, PORT);
+
+    try game.init(player);
+
+    var from: std.posix.sockaddr.in = undefined;
+
+    if (player == 0) {
+        try broadcastDiscovery(sock);
+
+        while (!rl.windowShouldClose()) {
+            if (listenForFriend(sock)) |ad| {
+                from = ad;
+
+                break;
+            }
+            std.debug.print("still waitin...\n", .{});
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    } else {
+        var from_len: std.posix.socklen_t = @sizeOf(std.posix.sockaddr.in);
+        var buf: [512]u8 = undefined;
+        while (!rl.windowShouldClose()) {
+            std.debug.print("waiting for broadcast,,..\n", .{});
+            const n = std.posix.recvfrom(
+                sock,
+                &buf,
+                0,
+                @ptrCast(&from),
+                &from_len,
+            ) catch |err| switch (err) {
+                error.WouldBlock => {
+                    continue;
+                },
+                else => {
+                    std.debug.print("recv error: {}\n", .{err});
+                    @panic("ERROR RECV");
+                },
+            };
+            std.debug.print("got my broadcaste: {s}\n", .{buf[0..n]});
+            break;
+        }
+
+        std.debug.print("... now sendin lesgo,,.. {any}\n", .{from});
+
+        _ = try std.posix.sendto(
+            sock,
+            "lesgo",
+            0,
+            @ptrCast(&from),
+            @sizeOf(std.posix.sockaddr.in),
+        );
+
+        std.debug.print("... sent!\n", .{});
+    }
+
+    var network_buttons = game.Buttons{};
+    var user_buttons = game.Buttons{};
+
+    while (!rl.windowShouldClose()) {
+        user_buttons.left_hit = rl.isKeyPressed(rl.KeyboardKey.left);
+        user_buttons.right_hit = rl.isKeyPressed(rl.KeyboardKey.right);
+        user_buttons.up_hit = rl.isKeyPressed(rl.KeyboardKey.up);
+        user_buttons.x_hit = rl.isKeyPressed(rl.KeyboardKey.x);
+        user_buttons.left_held = rl.isKeyDown(rl.KeyboardKey.left);
+        user_buttons.right_held = rl.isKeyDown(rl.KeyboardKey.right);
+        user_buttons.up_held = rl.isKeyDown(rl.KeyboardKey.up);
+        user_buttons.x_held = rl.isKeyDown(rl.KeyboardKey.x);
+
+        getNetworkButtons(sock, &network_buttons);
+
+        {
+            // sendNetworkButtons
+            _ = try std.posix.sendto(
+                sock,
+                std.mem.asBytes(&user_buttons),
+                0,
+                @ptrCast(&from),
+                @sizeOf(std.posix.sockaddr.in),
+            );
+        }
+
+        try game.update(user_buttons, network_buttons);
+
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.beginTextureMode(target);
+
+        try game.draw();
+
+        rl.endTextureMode();
+        const src = rl.Rectangle{
+            .x = 0,
+            .y = 0,
+            .width = @floatFromInt(target.texture.width),
+            .height = @floatFromInt(-target.texture.height),
+        };
+
+        const dst = rl.Rectangle{
+            .x = 0,
+            .y = 0,
+            .width = width * scale,
+            .height = height * scale,
+        };
+
+        rl.drawTexturePro(
+            target.texture,
+            src,
+            dst,
+            rl.Vector2.zero(),
+            0,
+            rl.Color.white,
+        );
+    }
 }
